0023f5e46264738d7c1e1a56c2dc284a5bac6f44
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-09 15:11:55 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+  }
+
+  render() {
+    return (
+      <FruitBasket />
+    )
+  }
+}
 
 export default App;
